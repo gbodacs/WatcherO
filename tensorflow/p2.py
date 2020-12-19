0f33402ea730dbcc459ab78891917c983642fa99
Gabor <gbodacs@gmail.com>
2020-12-19 22:22:38 +0100

    Auto stash before merge of "master" and "origin/master"

--- tensorflow/p2.py
+++ tensorflow/p2.py
@@ -9,6 +9,8 @@ from numpy import array
 from numpy import hstack
 import matplotlib.pyplot as plt
 from pandas import read_csv
+import datetime
+import glob, os
 import math
 import random
 import sys, getopt
@@ -24,47 +26,94 @@ from keras.layers import TimeDistributed
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_log_error
 
-class SaveData:
-    def __init__(self, data, cycle):
-        self.ticker = 
-        self.MSE = 
-        self.MAE = 
-        self.SGE = 
-        self.cycle = cycle
+class DataManager:
+    def __init__(self):
+        self.MSE = list()
+        self.MAE = list()
+        self.SLE = list()
 
-    def getData(self):
-        return self.data
+    def AddMSE(self, data, cycle):
+        self.MSE.append([data, cycle])
 
-    def checkBigger(self, data, cycle):
-        if (data>self.data):
-            self.data = data
-            self.cycle = cycle
+    def AddMAE(self, data, cycle):
+        self.MAE.append([data, cycle])
 
-    def checkSmaller(self, data, cycle):
-        if (data<self.data):
-            self.data = data
-            self.cycle = cycle
+    def AddSLE(self, data, cycle):
+        self.SLE.append([data, cycle])
 
-    def setDataCycle(self, data, cycle):
-        self.data = data
-        self.cycle = cycle
+    def Reset(self):
+        self.MSE.clear()
+        self.MAE.clear()
+        self.SLE.clear()
 
-    def printData(self, string):
-        print(f"{string} data: {self.data} at cycle: {self.cycle}")
+    def FinalizeAndPrint(self, fileName):
+        self.MSE.sort(key=lambda x:x[0])
+        self.MAE.sort(key=lambda x:x[0])
+        self.SLE.sort(key=lambda x:x[0])
 
-    # sort by name (Ascending order)
-    employees.sort(key=get_name)
-    print(employees, end='\n\n')
+        f = open(fileName+".txt", "w")
 
-    # sort by Age (Ascending order)
-    employees.sort(key=get_age)
-    print(employees, end='\n\n')
+        f.write("----------------------------")
+        for x in range(5):
+            f.write(f"{x+1}. MSE - {self.MSE[x][0]} at cycles: {self.MSE[x][1]}")
+        f.write("----------------------------")
+        for x in range(5):
+            f.write(f"{x+1}. MAE - {self.MAE[x][0]} at cycles: {self.MAE[x][1]}")
+        f.write("----------------------------")
+        for x in range(5):
+            f.write(f"{x+1}. SLE - {self.SLE[x][0]} at cycles: {self.SLE[x][1]}")
+        f.write("----------------------------")
+        f.close()
+
+dm = DataManager()
+
+def cycle_analysis(data, cycle, mode='additive', forecast_plot = False):
+    training = data[0:-300].iloc[:-1,]
+    testing = data[-300:]
+    predict_period = 1500#len(pd.date_range(split_date,max(data.index)))
+    df = training.reset_index()
+    df.columns = ['index','ds','y']
+    #m = Prophet(weekly_seasonality=False,yearly_seasonality=False,daily_seasonality=False)
+    #m.add_seasonality('self_define_cycle',period=cycle,fourier_order=32,mode=mode)
 
     # sort by salary (Descending order)
     employees.sort(key=get_salary, reverse=True)
     print(employees, end='\n\n')
 
+    m = Prophet(
+    growth="linear",
+    #holidays=holidays,
+    seasonality_mode=mode,
+    changepoint_prior_scale=30,
+    seasonality_prior_scale=35,
+    holidays_prior_scale=20,
+    daily_seasonality=False,
+    weekly_seasonality=False,
+    yearly_seasonality=False,
+    ).add_seasonality(
+        name='spec',
+        period=cycle,
+        fourier_order=32
+    #).add_seasonality(
+    #    name='daily',
+    #    period=1,
+    #    fourier_order=15
+    # ).add_seasonality(
+    #     name='weekly',
+    #     period=7,
+    #     fourier_order=20
+    # ).add_seasonality(
+    #     name='yearly',
+    #     period=365.25,
+    #     fourier_order=20
+    )
+    # .add_seasonality(
+    #     name='quarterly',
+    #     period=365.25/4,
+    #     fourier_order=32,
+    #     prior_scale=15)
 
 MIN_mse = SaveData(20000, 0)
 MIN_mae = SaveData(20000, 0)
@@ -86,28 +135,27 @@ def cycle_analysis(data, split_date, cycle,mode='additive', forecast_plot = Fals
         plt.xlabel('Date',fontsize=12,fontweight='bold',color='gray')
         plt.ylabel('Price',fontsize=12,fontweight='bold',color='gray')
         plt.show()
-    ret = max(forecast.self_define_cycle)-min(forecast.self_define_cycle)
-    model_tb = forecast['yhat']
-    model_tb.index = forecast['ds'].map(lambda x:x.strftime("%Y-%m-%d"))
     
-
-    Rpc = round(ret,3)
-    Mse = mean_squared_error(training["y"], forecast['yhat'])
-    Mae = mean_absolute_error(training["y"], forecast['yhat'])        
+    temp = forecast['yhat']
+    Mse = mean_squared_error(training["y"], temp[0:len(training["y"])] )
+    Mae = mean_absolute_error(training["y"], temp[0:len(training["y"])] )
+    Sle = mean_squared_log_error(training["y"], temp[0:len(training["y"])] )
     
-    MIN_mse.checkSmaller(Mse, cycle)
-    MIN_mae.checkSmaller(Mae, cycle)
+    dm.AddMSE(Mse, cycle)
+    dm.AddMAE(Mae, cycle)
+    dm.AddSLE(Sle, cycle)
 
     return 0
 
-df = pd.read_csv('VIX_daily.csv', usecols=[0,4])
-df.head()
 
-for i in range(282,284):
-    cycle_analysis(df, '2017-01-01', i, 'additive', forecast_plot=True)
-
-MIN_mse.printData("MSE minimum ")
-MIN_mae.printData("MAE minimum ")
+os.chdir("./data")
+for fileName in glob.glob("*.csv"):
+    df = pd.read_csv(fileName, usecols=[0,4])
+    dm.Reset()
+    for i in range(10,370):
+        cycle_analysis(df, i, 'additive', forecast_plot=False)
+    print(fileName)
+    dm.FinalizeAndPrint(fileName)
 
 print("Done.")
 
--- tensorflow/p_test.py
+++ tensorflow/p_test.py
@@ -4,5 +4,57 @@ import numpy as np
 import datetime
 import glob
 
-for file in glob.glob("*.csv"):
-    print(file)
+class DataManager:
+    def __init__(self):
+        self.MSE = list()
+        self.MAE = list()
+        self.SGE = list()
+
+    def AddMSE(self, data, cycle):
+        self.MSE.append([data, cycle])
+
+    def AddMAE(self, data, cycle):
+        self.MAE.append([data, cycle])
+
+    def AddSGE(self, data, cycle):
+        self.SGE.append([data, cycle])
+
+    def FinalizeAndPrint(self):
+        self.MSE.sort(key=lambda x:x[0])
+        self.MAE.sort(key=lambda x:x[0])
+        self.SGE.sort(key=lambda x:x[0])
+
+        print("----------------------------")
+        for x in range(5):
+            print(f"{x+1}. MSE - {self.MSE[x][0]} at cycles: {self.MSE[x][1]}")
+        print("----------------------------")
+        for x in range(5):
+            print(f"{x+1}. MAE - {self.MAE[x][0]} at cycles: {self.MAE[x][1]}")
+        print("----------------------------")
+        for x in range(5):
+            print(f"{x+1}. SGE - {self.SGE[x][0]} at cycles: {self.SGE[x][1]}")
+        print("----------------------------")
+
+dm = DataManager()
+dm.AddMAE(1,22)
+dm.AddMAE(99,23)
+dm.AddMAE(1111,26)
+dm.AddMAE(9,2)
+dm.AddMAE(19,7)
+dm.AddMAE(2,5)
+
+dm.AddMSE(1,22)
+dm.AddMSE(29,23)
+dm.AddMSE(111,26)
+dm.AddMSE(11,2)
+dm.AddMSE(19,7)
+dm.AddMSE(2,5)
+
+dm.AddSGE(10,22)
+dm.AddSGE(909,23)
+dm.AddSGE(10111,26)
+dm.AddSGE(90,2)
+dm.AddSGE(109,7)
+dm.AddSGE(20,5)
+
+dm.FinalizeAndPrint()
